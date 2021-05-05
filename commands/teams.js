2aad78d213ebdbb953431194b298955452d19810
Daniel Poss <dpatposs@msn.com>
2021-05-05 09:14:04 -0500

    Merging from Master

--- commands/teams.js
+++ commands/teams.js
@@ -1,21 +1,25 @@
-const dbUtils = require('../team-impl');
+const dbUtils = require('../dao/dynamo-db-impl');
 const teamsCard = require('../templates/teams');
 module.exports = {
     name: 'teams',
     description: 'Used to return a human readable response of the current Clash team status.',
-    execute: async function (msg, args) {
-        let teams = await dbUtils.getTeams();
-        let tentative = await dbUtils.getTentative();
-        let copy = JSON.parse(JSON.stringify(teamsCard));
-        if (teams.length !== 0) {
-            teams.forEach((team) => {
-                copy.fields.push({name: team.name, value: team.players});
-            });
-        } if (tentative.length > 0) {
-            copy.fields.push({name: 'Tentative Queue', value: tentative});
-        } else {
-            copy.fields.push({name: 'No Existing Teams. Please register!', value: 'Emptay'})
-        }
-        msg.reply({embed: copy});
+    execute: async function (msg) {
+        dbUtils.getTeams(msg.guild.name).then(data => {
+            let tentative = dbUtils.getTentative();
+            let copy = JSON.parse(JSON.stringify(teamsCard));
+            if (data.length !== 0) {
+                data.forEach((team) => {
+                    copy.fields.push({name: team.name, value: team.players});
+                });
+            } if (tentative.length > 0) {
+                copy.fields.push({name: 'Tentative Queue', value: tentative});
+            } else {
+                copy.fields.push({name: 'No Existing Teams. Please register!', value: 'Emptay'})
+            }
+            msg.reply({embed: copy});
+        }).catch(err => {
+            console.error(err);
+            msg.reply('Failed to retrieve the current Clash Teams status. Please reach out to <@299370234228506627>.')
+        });
     },
 };
