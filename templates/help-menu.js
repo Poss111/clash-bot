5b279a3902ba0e9e057e40e15e7539cd80d5c929
Daniel Poss <dpatposs@msn.com>
2021-05-06 10:32:58 -0500

    Updating verbiage of the help-menu to include that tentative queue is not persisted and added the issue tracker link for users to suggest improvements.

--- templates/help-menu.js
+++ templates/help-menu.js
@@ -1,7 +1,7 @@
 module.exports = {
     title: "Clash Bot Helpful Commands! :D",
     color: 0x071A32,
-    description: "Help is on the way! Here are a few quick commands to get you started.\n***Note***: Use !clash at the beginning of each of your commands to make use of this bot. i.e. !clash help",
+    description: "Help is on the way! Here are a few quick commands to get you started.\n***Note***: Use !clash at the beginning of each of your commands to make use of this bot. i.e. !clash help\nAny improvements to be suggested, please create an issue at the following location [Clash Bot Issues](https://github.com/Poss111/clash-bot/issues)",
     fields: [
         {
             name: "ping",
@@ -25,7 +25,7 @@ module.exports = {
         },
         {
             name: "tentative",
-            value: "Asks the bot to place your name on the tentative queue, if you are already belong in the queue, you will be removed from it. You will be removed from a team if you have been assigned one."
+            value: "Asks the bot to place your name on the tentative queue, if you are already belong in the queue, you will be removed from it. You will be removed from a team if you have been assigned one. ***Note***: This queue is not maintain so you will be removed from the queue upon update of the bot."
         },
         {
             name: "time",
