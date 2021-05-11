61be1458b99d2891f25451ca438a83f1018f5690
Daniel Poss <dpatposs@msn.com>
2021-05-11 09:05:15 -0500

    Covered teams command with unit tests

--- commands/teams.js
+++ commands/teams.js
@@ -4,21 +4,27 @@ const errorHandler = require('../utility/error-handling');
 module.exports = {
     name: 'teams',
     description: 'Used to return a human readable response of the current Clash team status.',
-    execute: async function (msg) {
+    execute: async function (msg, callback) {
         dbUtils.getTeams(msg.guild.name).then(data => {
             let tentative = dbUtils.getTentative();
             let copy = JSON.parse(JSON.stringify(teamsCard));
-            data.forEach((team) => {
-                if (team.players && team.players.length > 0) {
-                    copy.fields.push({name: team.teamName, value: team.players});
-                }
-            });
-            if (copy.fields.length <= 0) {
+
+            if (!data || data.length <= 0) {
                 copy.fields.push({name: 'No Existing Teams. Please register!', value: 'Emptay'})
-            } if (tentative.length > 0) {
+            } else {
+                data.forEach((team) => {
+                    if (team.players && team.players.length > 0) {
+                        copy.fields.push({name: team.teamName, value: team.players});
+                    }
+                });
+            } if (tentative && tentative.length > 0) {
                 copy.fields.push({name: 'Tentative Queue', value: tentative});
             }
             msg.reply({embed: copy});
-        }).catch(err => errorHandler.handleError(this.name, err, msg, 'Failed to retrieve the current Clash Teams status.'));
+            return callback();
+        }).catch(err => {
+            errorHandler.handleError(this.name, err, msg, 'Failed to retrieve the current Clash Teams status.')
+            return callback();
+        });
     },
 };
---
+++ commands/teams.test.js
@@ -0,0 +1,176 @@
+const teams = require('./teams');
+const dynamoDBUtils = require('../dao/dynamo-db-impl');
+const errorHandling = require('../utility/error-handling');
+
+jest.mock('../dao/dynamo-db-impl');
+jest.mock('../utility/error-handling');
+
+test('When a team is passed back, it should be populated as a field in the embedded property of the reply.', (done) => {
+    let messagePassed = '';
+    let msg = {
+        reply: (value) => messagePassed = value,
+        author: {
+            username: 'TestPlayer'
+        },
+        guild: {
+            name: 'TestServer'
+        }
+    };
+    const sampleTeamTwoPlayers = [{
+        key: `TestTeam#${msg.guild.name}`,
+        teamName: 'TestTeam',
+        serverName: `${msg.guild.name}`,
+        players: ['Player1', 'Player2']
+    }];
+    function callback() {
+        try {
+            expect(messagePassed.embed.fields.length).toEqual(1);
+            expect(messagePassed.embed.fields[0].name).toEqual(sampleTeamTwoPlayers[0].teamName);
+            expect(messagePassed.embed.fields[0].value).toEqual(sampleTeamTwoPlayers[0].players);
+            done();
+        } catch(error) {
+            done(error);
+        }
+    }
+    dynamoDBUtils.getTeams.mockResolvedValue(sampleTeamTwoPlayers);
+    teams.execute(msg, callback);
+})
+
+test('When no teams are passed back, it should be populate the not existing teams message.', (done) => {
+    let messagePassed = '';
+    let msg = {
+        reply: (value) => messagePassed = value,
+        author: {
+            username: 'TestPlayer'
+        },
+        guild: {
+            name: 'TestServer'
+        }
+    };
+    const sampleTeamTwoPlayers = [];
+    function callback() {
+        try {
+            expect(messagePassed.embed.fields.length).toEqual(1);
+            expect(messagePassed.embed.fields[0].name).toEqual('No Existing Teams. Please register!');
+            expect(messagePassed.embed.fields[0].value).toEqual('Emptay');
+            done();
+        } catch(error) {
+            done(error);
+        }
+    }
+    dynamoDBUtils.getTeams.mockResolvedValue(sampleTeamTwoPlayers);
+    teams.execute(msg, callback);
+})
+
+test('When undefined teams are passed back, it should be populate the not existing teams message.', (done) => {
+    let messagePassed = '';
+    let msg = {
+        reply: (value) => messagePassed = value,
+        author: {
+            username: 'TestPlayer'
+        },
+        guild: {
+            name: 'TestServer'
+        }
+    };
+    function callback() {
+        try {
+            expect(messagePassed.embed.fields.length).toEqual(1);
+            expect(messagePassed.embed.fields[0].name).toEqual('No Existing Teams. Please register!');
+            expect(messagePassed.embed.fields[0].value).toEqual('Emptay');
+            done();
+        } catch(error) {
+            done(error);
+        }
+    }
+    dynamoDBUtils.getTeams.mockResolvedValue(undefined);
+    teams.execute(msg, callback);
+})
+
+test('When tentative players and no teams are passed back, it should populate the tentative list with an empty teams message.', (done) => {
+    let messagePassed = '';
+    let msg = {
+        reply: (value) => messagePassed = value,
+        author: {
+            username: 'TestPlayer'
+        },
+        guild: {
+            name: 'TestServer'
+        }
+    };
+    const sampleTeamTwoPlayers = [];
+    const sampleTentativeList = ['Player1'];
+    function callback() {
+        try {
+            expect(messagePassed.embed.fields.length).toEqual(2);
+            expect(messagePassed.embed.fields[0].name).toEqual('No Existing Teams. Please register!');
+            expect(messagePassed.embed.fields[0].value).toEqual('Emptay');
+            expect(messagePassed.embed.fields[1].name).toEqual('Tentative Queue');
+            expect(messagePassed.embed.fields[1].value).toEqual(sampleTentativeList);
+            done();
+        } catch(error) {
+            done(error);
+        }
+    }
+    dynamoDBUtils.getTeams.mockResolvedValue(sampleTeamTwoPlayers);
+    dynamoDBUtils.getTentative.mockReturnValue(sampleTentativeList);
+    teams.execute(msg, callback);
+})
+
+test('When tentative players and a team are passed back, it should populate the tentative list with the existing team.', (done) => {
+    let messagePassed = '';
+    let msg = {
+        reply: (value) => messagePassed = value,
+        author: {
+            username: 'TestPlayer'
+        },
+        guild: {
+            name: 'TestServer'
+        }
+    };
+    const sampleTeamTwoPlayers = [{
+        key: `TestTeam#${msg.guild.name}`,
+        teamName: 'TestTeam',
+        serverName: `${msg.guild.name}`,
+        players: ['Player1', 'Player2']
+    }];
+    const sampleTentativeList = ['Player1'];
+    function callback() {
+        try {
+            expect(messagePassed.embed.fields.length).toEqual(2);
+            expect(messagePassed.embed.fields[0].name).toEqual(sampleTeamTwoPlayers[0].teamName);
+            expect(messagePassed.embed.fields[0].value).toEqual(sampleTeamTwoPlayers[0].players);
+            expect(messagePassed.embed.fields[1].name).toEqual('Tentative Queue');
+            expect(messagePassed.embed.fields[1].value).toEqual(sampleTentativeList);
+            done();
+        } catch(error) {
+            done(error);
+        }
+    }
+    dynamoDBUtils.getTeams.mockResolvedValue(sampleTeamTwoPlayers);
+    dynamoDBUtils.getTentative.mockReturnValue(sampleTentativeList);
+    teams.execute(msg, callback);
+})
+
+test('If an error occurs, the error handler will be invoked.', (done) => {
+    let messagePassed = '';
+    let msg = {
+        reply: (value) => messagePassed = value,
+        author: {
+            username: 'TestPlayer'
+        },
+        guild: {
+            name: 'TestServer'
+        }
+    };
+    function callback() {
+        try {
+            done();
+        } catch(error) {
+            expect(errorHandling.mock.calls.length).toEqual(1);
+            done(error);
+        }
+    }
+    dynamoDBUtils.getTeams.mockRejectedValue('Some error occurred.');
+    teams.execute(msg, callback);
+})
