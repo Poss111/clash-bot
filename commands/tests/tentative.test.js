9df74b59412e3a8f5f2d2d878191b26c97719cd6
Daniel Poss <dpatposs@msn.com>
2021-05-21 06:02:56 -0500

    Updated tentative to require a single argument pass - tournament name. This will be verified against the list of tournament names stored during runtime.s

--- commands/tests/tentative.test.js
+++ commands/tests/tentative.test.js
@@ -1,9 +1,11 @@
 const tentative = require('../tentative');
 const dynamoDBUtils = require('../../dao/dynamo-db-impl');
 const errorHandling = require('../../utility/error-handling');
+const leagueApi = require('../../utility/LeagueApi');
 
 jest.mock('../../dao/dynamo-db-impl');
 jest.mock('../../utility/error-handling');
+jest.mock('../../utility/LeagueApi');
 
 test('Should respond with user has been placed on tentative if the player name does not exist in the tentative list.', async () => {
     let messagePassed = '';
@@ -16,8 +18,25 @@ test('Should respond with user has been placed on tentative if the player name d
             name: 'TestServer'
         }
     };
+    let leagueTimes = [
+        {
+            tournamentName: "msi2021",
+            tournamentDay: "day_3",
+            "startTime": "May 29 2021 07:00 pm PDT",
+            "registrationTime": "May 29 2021 04:15 pm PDT"
+        },
+        {
+            tournamentName: "msi2021",
+            tournamentDay: "day_4",
+            "startTime": "May 30 2021 07:00 pm PDT",
+            "registrationTime": "May 30 2021 04:15 pm PDT"
+        }
+    ];
+    leagueApi.findTournament.mockReturnValue(leagueTimes);
     dynamoDBUtils.handleTentative.mockResolvedValue(false);
-    await tentative.execute(msg);
+    let args = ['msi2021'];
+    await tentative.execute(msg, args);
+    expect(dynamoDBUtils.handleTentative).toBeCalledWith(msg.author.username, msg.guild.name, args[0]);
     expect(messagePassed).toEqual(`We placed you into the tentative queue. If you were on a team, you have been removed. tip: Use '!clash teams' to view current team status`);
 })
 
@@ -32,9 +51,96 @@ test('Should respond with user has been taken off tentative if the player name d
             name: 'TestServer'
         }
     };
+    let leagueTimes = [
+        {
+            tournamentName: "msi2021",
+            tournamentDay: "day_3",
+            "startTime": "May 29 2021 07:00 pm PDT",
+            "registrationTime": "May 29 2021 04:15 pm PDT"
+        },
+        {
+            tournamentName: "msi2021",
+            tournamentDay: "day_4",
+            "startTime": "May 30 2021 07:00 pm PDT",
+            "registrationTime": "May 30 2021 04:15 pm PDT"
+        }
+    ];
+    leagueApi.findTournament.mockReturnValue(leagueTimes);
     dynamoDBUtils.handleTentative.mockResolvedValue(true);
-    await tentative.execute(msg);
+    let args = ['msi2021'];
+    await tentative.execute(msg, args);
+    expect(dynamoDBUtils.handleTentative).toBeCalledWith(msg.author.username, msg.guild.name, args[0]);
     expect(messagePassed).toEqual(`We have taken you off of tentative queue. tip: Use '!clash teams' to view current team status`);
+})
+
+test('If tournament passed by user is not found and return as undefined, the user should be notified.', async () => {
+    let messagePassed = '';
+    let msg = {
+        reply: (value) => messagePassed = value,
+        author: {
+            username: 'TestPlayer'
+        },
+        guild: {
+            name: 'TestServer'
+        }
+    };
+    leagueApi.findTournament.mockReturnValue(undefined);
+    let args = ['msi2021'];
+    await tentative.execute(msg, args);
+    expect(leagueApi.findTournament).toBeCalledWith(args[0]);
+    expect(messagePassed).toEqual('Cannot find the tournament passed. Please check !clash time for an appropriate list.');
+})
+
+test('If tournament passed by user is not found and return as empty, the user should be notified.', async () => {
+    let messagePassed = '';
+    let msg = {
+        reply: (value) => messagePassed = value,
+        author: {
+            username: 'TestPlayer'
+        },
+        guild: {
+            name: 'TestServer'
+        }
+    };
+    leagueApi.findTournament.mockReturnValue([]);
+    let args = ['msi2021'];
+    await tentative.execute(msg, args);
+    expect(leagueApi.findTournament).toBeCalledWith(args[0]);
+    expect(messagePassed).toEqual('Cannot find the tournament passed. Please check !clash time for an appropriate list.');
+})
+
+test('Should require a tournament as an argument.', async () => {
+    let messagePassed = '';
+    let msg = {
+        reply: (value) => messagePassed = value,
+        author: {
+            username: 'TestPlayer'
+        },
+        guild: {
+            name: 'TestServer'
+        }
+    };
+    let args = [];
+    dynamoDBUtils.handleTentative.mockResolvedValue(true);
+    await tentative.execute(msg, args);
+    expect(messagePassed).toEqual(`A tournament name to be tentative for is missing. Please use !clash tentative 'tournament name' to use tentative.`);
+})
+
+test('Should require a single argument.', async () => {
+    let messagePassed = '';
+    let msg = {
+        reply: (value) => messagePassed = value,
+        author: {
+            username: 'TestPlayer'
+        },
+        guild: {
+            name: 'TestServer'
+        }
+    };
+    let args = undefined;
+    dynamoDBUtils.handleTentative.mockResolvedValue(true);
+    await tentative.execute(msg, args);
+    expect(messagePassed).toEqual(`A tournament name to be tentative for is missing. Please use !clash tentative 'tournament name' to use tentative.`);
 })
 
 test('If an error occurs, the error handler will be invoked.', async () => {
@@ -49,7 +155,23 @@ test('If an error occurs, the error handler will be invoked.', async () => {
             name: 'TestServer'
         }
     };
+    let leagueTimes = [
+        {
+            tournamentName: "msi2021",
+            tournamentDay: "day_3",
+            "startTime": "May 29 2021 07:00 pm PDT",
+            "registrationTime": "May 29 2021 04:15 pm PDT"
+        },
+        {
+            tournamentName: "msi2021",
+            tournamentDay: "day_4",
+            "startTime": "May 30 2021 07:00 pm PDT",
+            "registrationTime": "May 30 2021 04:15 pm PDT"
+        }
+    ];
+    leagueApi.findTournament.mockReturnValue(leagueTimes);
+    let args = ['msi2021'];
     dynamoDBUtils.handleTentative.mockRejectedValue('Some error occurred.');
-    await tentative.execute(msg);
+    await tentative.execute(msg, args);
     expect(errorHandling.handleError.mock.calls.length).toEqual(1);
 })
