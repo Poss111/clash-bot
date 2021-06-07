c21723338a1795d679130a757873c4fa51c8d46c
Daniel Poss <dpatposs@msn.com>
2021-06-07 08:32:42 -0500

    Updated clashtime-db-impl.js to retrieve results from dynamodb on every request for now. Had to refactor to use promise for filtering league times.

--- dao/tests/clashtime-db-impl.test.js
+++ dao/tests/clashtime-db-impl.test.js
@@ -22,12 +22,12 @@ test('Should return a parsed array of human readable dates from the League Clash
             }
         }, {
             attrs: {
-                    key: "msi2021#4",
-                    tournamentName: "msi2021",
-                    tournamentDay: "3",
-                    registrationTime: "June 12 2021 04:15 pm PDT",
-                    startTime: "June 12 2021 05:15 pm PDT",
-                }
+                key: "msi2021#4",
+                tournamentName: "msi2021",
+                tournamentDay: "3",
+                registrationTime: "June 12 2021 04:15 pm PDT",
+                startTime: "June 12 2021 05:15 pm PDT",
+            }
         }]
     };
     const mockStream = jest.fn().mockImplementation(() => streamTest.v2.fromObjects([value]));
@@ -43,7 +43,7 @@ test('Should return a parsed array of human readable dates from the League Clash
     value.Items.forEach(record => {
         expectedData.push(JSON.parse(JSON.stringify(record.attrs)));
     });
-    expectedData.sort((a,b) => parseInt(a.tournamentDay) - parseInt(b.tournamentDay));
+    expectedData.sort((a, b) => parseInt(a.tournamentDay) - parseInt(b.tournamentDay));
 
     return clashtimeDb.initializeLeagueData().then(data => {
         expect(data).toEqual(expectedData);
@@ -57,193 +57,149 @@ test('Should be able to set time if correctData is passed.', () => {
 })
 
 describe('Find Tournament', () => {
-    test('I should be able to search for a tournament by its exact name and it should be returned if matching.', () => {
+
+    function buildSampleTournamentList(tournamentName, days, daysBehind) {
+        let tournaments = [];
+        let expectedTournaments = [];
         const dateFormat = 'MMMM DD yyyy hh:mm a z';
-        let currentDate = new moment().add(1, 'hour').format(dateFormat);
-        let nextDate = new moment().add(1, 'hour').add(1, 'day').format(dateFormat);
-        clashtimeDb.leagueTimes = [
-            {
-                tournamentName: "msi2021",
-                tournamentDay: "3",
-                startTime: currentDate,
-                registrationTime: currentDate
-            },
-            {
-                tournamentName: "msi2021",
-                tournamentDay: "4",
-                startTime: nextDate,
-                registrationTime: nextDate
+        for (let i = 0; i < days; i++) {
+            if (!daysBehind) {
+                daysBehind = 0;
             }
-        ];
-        expect(clashtimeDb.findTournament('msi2021')).toEqual(clashtimeDb.leagueTimes);
+            let firstDate = new moment()
+                .add(i - daysBehind, 'day').format(dateFormat);
+            let secondDate = new moment()
+                .add(i - daysBehind, 'day')
+                .add(1, 'hour').format(dateFormat);
+            let tournament = {
+                key: `${tournamentName}#${i}`,
+                tournamentName: tournamentName,
+                tournamentDay: `${i}`,
+                registrationTime: firstDate,
+                startTime: secondDate,
+            };
+            tournaments.push(
+                {
+                    attrs: tournament
+                }
+            )
+            expectedTournaments.push(tournament);
+        }
+        const value = {Items: tournaments};
+
+        const mockStream = jest.fn().mockImplementation(() => streamTest.v2.fromObjects([value]));
+        clashtimeDb.clashTimesTable = {
+            scan: jest.fn().mockReturnThis(),
+            filterExpression: jest.fn().mockReturnThis(),
+            expressionAttributeValues: jest.fn().mockReturnThis(),
+            expressionAttributeNames: jest.fn().mockReturnThis(),
+            exec: mockStream
+        };
+        return expectedTournaments;
+    }
+
+    test('I should be able to search for a tournament by its exact name and it should be returned if matching.', () => {
+        let expectedData = buildSampleTournamentList('msi2021', 2);
+        return clashtimeDb.findTournament('msi2021').then(data => {
+            expect(data).toEqual(expectedData);
+        })
     })
 
     test('I should be returned an empty value if a match is not found.', () => {
-        clashtimeDb.leagueTimes = [
-            {
-                tournamentName: "msi2021",
-                tournamentDay: "3",
-                "startTime": "May 29 2021 07:00 pm PDT",
-                "registrationTime": "May 29 2021 04:15 pm PDT"
-            },
-            {
-                tournamentName: "msi2021",
-                tournamentDay: "4",
-                "startTime": "May 30 2021 07:00 pm PDT",
-                "registrationTime": "May 30 2021 04:15 pm PDT"
-            }
-        ];
-        expect(clashtimeDb.findTournament('abcde')).toHaveLength(0);
+        buildSampleTournamentList('msi2021', 2);
+        return clashtimeDb.findTournament('abcde').then(data => {
+            expect(data).toHaveLength(0);
+        });
     })
 
     test('I should be returned an empty value if a tournament name match is not found due to date being in the past.', () => {
-        const currentDate = new Date();
-        currentDate.setDate(currentDate.getDate() - 1);
-        const currentDateTwo = new Date();
-        currentDateTwo.setDate(currentDateTwo.getDate() - 2);
-        clashtimeDb.leagueTimes = [
-            {
-                tournamentName: "msi2021",
-                tournamentDay: "3",
-                "startTime": currentDate,
-                "registrationTime": "May 29 2021 04:15 pm PDT"
-            },
-            {
-                tournamentName: "msi2021",
-                tournamentDay: "4",
-                "startTime": currentDateTwo,
-                "registrationTime": "May 30 2021 04:15 pm PDT"
-            }
-        ];
-        expect(clashtimeDb.findTournament('msi2021')).toHaveLength(0);
+        buildSampleTournamentList('msi2021', 2, 2);
+        return clashtimeDb.findTournament('msi2021').then(data => {
+            expect(data).toHaveLength(0);
+        });
     })
 
     test('I should be returned an empty value if a tournament name and day match is not found due to date being in the past.', () => {
-        const currentDate = new Date();
-        currentDate.setDate(currentDate.getDate() - 1);
-        const currentDateTwo = new Date();
-        currentDateTwo.setDate(currentDateTwo.getDate() - 2);
-        clashtimeDb.leagueTimes = [
-            {
-                tournamentName: "msi2021",
-                tournamentDay: "3",
-                "startTime": currentDate,
-                "registrationTime": "May 29 2021 04:15 pm PDT"
-            },
-            {
-                tournamentName: "msi2021",
-                tournamentDay: "4",
-                "startTime": currentDateTwo,
-                "registrationTime": "May 30 2021 04:15 pm PDT"
-            }
-        ];
-        expect(clashtimeDb.findTournament('msi2021', '3')).toHaveLength(0);
+        buildSampleTournamentList('msi2021', 2, 2);
+        return clashtimeDb.findTournament('msi2021', 2).then(data => {
+            expect(data).toHaveLength(0);
+        });
     })
 
     test('I should be able to search for a tournament and a day.', () => {
-        const dateFormat = 'MMMM DD yyyy hh:mm a z';
-        let currentDate = new moment().add(1, 'hour').format(dateFormat);
-        let nextDate = new moment().add(1, 'hour').add(1, 'day').format(dateFormat);
-        clashtimeDb.leagueTimes = [
-            {
-                tournamentName: "msi2021",
-                tournamentDay: "3",
-                startTime: currentDate,
-                registrationTime: currentDate
-            },
-            {
-                tournamentName: "msi2021",
-                tournamentDay: "4",
-                startTime: nextDate,
-                registrationTime: nextDate
-            }
-        ];
-        expect(clashtimeDb.findTournament('msi2021', '4')).toEqual([clashtimeDb.leagueTimes[1]]);
+        let expectedData = buildSampleTournamentList('msi2021', 2);
+        return clashtimeDb.findTournament('msi2021', 0).then(data => {
+            expect(data).toEqual([expectedData[0]]);
+        })
     })
 
     test('I should be able to search for a partial name of a tournament.', () => {
-        const dateFormat = 'MMMM DD yyyy hh:mm a z';
-        let currentDate = new moment().add(1, 'hour').format(dateFormat);
-        let nextDate = new moment().add(1, 'hour').add(1, 'day').format(dateFormat);
-        clashtimeDb.leagueTimes = [
-            {
-                tournamentName: "msi2021",
-                tournamentDay: "3",
-                "startTime": currentDate,
-                "registrationTime": currentDate
-            },
-            {
-                tournamentName: "msi2021",
-                tournamentDay: "4",
-                "startTime": nextDate,
-                "registrationTime": nextDate
-            }
-        ];
-        expect(clashtimeDb.findTournament('msi')).toEqual(clashtimeDb.leagueTimes);
+        let expectedData = buildSampleTournamentList('msi2021', 2);
+        return clashtimeDb.findTournament('msi').then(data => {
+            expect(data).toEqual(expectedData);
+        })
     })
 
     test('I should be able to search for a partial name and regardless of case for a tournament.', () => {
-        const dateFormat = 'MMMM DD yyyy hh:mm a z';
-        let currentDate = new moment().add(1, 'hour').format(dateFormat);
-        let nextDate = new moment().add(1, 'hour').add(1, 'day').format(dateFormat);
-        clashtimeDb.leagueTimes = [
-            {
-                tournamentName: "msi2021",
-                tournamentDay: "3",
-                "startTime": currentDate,
-                "registrationTime": currentDate
-            },
-            {
-                tournamentName: "msi2021",
-                tournamentDay: "4",
-                "startTime": nextDate,
-                "registrationTime": nextDate
-            }
-        ];
-        expect(clashtimeDb.findTournament('MSI')).toEqual(clashtimeDb.leagueTimes);
+        let expectedData = buildSampleTournamentList('msi2021', 2);
+        return clashtimeDb.findTournament('MSI').then(data => {
+            expect(data).toEqual(expectedData);
+        })
     })
 
     test('I should be able to return the all available tournaments based on the current date if nothing is passed', () => {
-        const currentDate = new Date();
-        currentDate.setDate(currentDate.getDate() - 1);
-        const currentDateTwo = new Date();
-        currentDateTwo.setDate(currentDateTwo.getDate() + 1);
-        clashtimeDb.leagueTimes = [
-            {
-                tournamentName: "msi2021",
-                tournamentDay: "3",
-                "startTime": currentDate,
-                "registrationTime": "May 29 2021 04:15 pm PDT"
-            },
-            {
-                tournamentName: "msi2021",
-                tournamentDay: "4",
-                "startTime": currentDateTwo.toDateString(),
-                "registrationTime": "May 30 2021 04:15 pm PDT"
-            }
-        ];
-        expect(clashtimeDb.findTournament()).toEqual([clashtimeDb.leagueTimes[1]]);
+        let expectedData = buildSampleTournamentList('msi2021', 2);
+        return clashtimeDb.findTournament().then(data => {
+            expect(data).toEqual(expectedData);
+        })
     })
 
     test('I should be able to return no tournaments if there are none available for the current date if nothing is passed.', () => {
-        const currentDate = new Date();
-        currentDate.setDate(currentDate.getDate() - 2);
-        const currentDateTwo = new Date();
-        currentDateTwo.setDate(currentDateTwo.getDate() - 1);
-        clashtimeDb.leagueTimes = [
-            {
-                tournamentName: "msi2021",
-                tournamentDay: "3",
-                "startTime": currentDate,
-                "registrationTime": "May 29 2021 04:15 pm PDT"
-            },
-            {
-                1: "msi2021",
-                tournamentDay: "4",
-                "startTime": currentDateTwo,
-                "registrationTime": "May 30 2021 04:15 pm PDT"
-            }
-        ];
-        expect(clashtimeDb.findTournament()).toHaveLength(0);
+        buildSampleTournamentList('msi2021', 2, 2);
+        return clashtimeDb.findTournament().then(data => {
+            expect(data).toHaveLength(0);
+        })
+    })
+})
+
+describe('Retrieve Tournaments', () => {
+    test('Should be able to retrieve all tournaments from stream and should be sorted by greatest to least day number.', () => {
+        process.env.TOKEN = 'testToken';
+        const value = {
+            Items: [{
+                attrs: {
+                    key: "msi2021#3",
+                    tournamentName: "msi2021",
+                    tournamentDay: "4",
+                    registrationTime: "June 13 2021 04:15 pm PDT",
+                    startTime: "June 13 2021 05:15 pm PDT"
+                }
+            }, {
+                attrs: {
+                    key: "msi2021#4",
+                    tournamentName: "msi2021",
+                    tournamentDay: "3",
+                    registrationTime: "June 12 2021 04:15 pm PDT",
+                    startTime: "June 12 2021 05:15 pm PDT",
+                }
+            }]
+        };
+        const mockStream = jest.fn().mockImplementation(() => streamTest.v2.fromObjects([value]));
+        clashtimeDb.clashTimesTable = {
+            scan: jest.fn().mockReturnThis(),
+            filterExpression: jest.fn().mockReturnThis(),
+            expressionAttributeValues: jest.fn().mockReturnThis(),
+            expressionAttributeNames: jest.fn().mockReturnThis(),
+            exec: mockStream
+        };
+        let expectedData = [];
+        value.Items.forEach(record => {
+            expectedData.push(JSON.parse(JSON.stringify(record.attrs)));
+        });
+        expectedData.sort((a, b) => parseInt(a.tournamentDay) - parseInt(b.tournamentDay));
+
+        return clashtimeDb.retrieveTournaments().then(data => {
+            expect(data).toEqual(expectedData);
+        });
     })
 })
