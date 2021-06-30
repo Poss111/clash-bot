13c3953b29e40dfb8cb98ffe70a3719143737618
Daniel Poss <dpatposs@msn.com>
2021-06-30 05:47:27 -0500

    - Added class to parse arguments in any order for user ease of use

---
+++ commands/command-argument-parser.js
@@ -0,0 +1,20 @@
+class CommandArgumentParser {
+
+    parse(args) {
+        let commands = {};
+        args.forEach(record => {
+            let re = new RegExp('^[0-9]{0,2}$');
+            if (record === 'newTeam') {
+                commands.createNewTeam = true;
+            } else if (re.test(record)) {
+                commands.tournamentDay = record;
+            } else {
+                commands.tournamentName = record;
+            }
+        })
+        return commands;
+    }
+
+}
+
+module.exports = new CommandArgumentParser;
