8e32992515a487fe61bd01e0463ca4c964941245
Daniel Poss <dpatposs@msn.com>
2021-07-15 10:42:30 -0500

    - clash-subscription-db-impl.js - Adding table logic for subscription

--- dao/clash-subscription-db-impl.js
+++ dao/clash-subscription-db-impl.js
@@ -1,4 +1,5 @@
 const dynamodb = require('dynamodb');
+const Joi = require('joi');
 
 class ClashSubscriptionDbImpl {
 
@@ -7,30 +8,56 @@ class ClashSubscriptionDbImpl {
 
     initialize() {
         return new Promise((resolve, reject) => {
-            if (process.env.LOCAL) {
-                console.log('Loading credentials from local.');
-                dynamodb.AWS.config.loadFromPath('./credentials.json');
-            } else {
-                console.log('Loading credentials from remote.');
-                dynamodb.AWS.config.update({
-                    region: `${process.env.REGION}`
+            try {
+                if (process.env.LOCAL) {
+                    console.log('Loading credentials from local.');
+                    dynamodb.AWS.config.loadFromPath('./credentials.json');
+                } else {
+                    console.log('Loading credentials from remote.');
+                    dynamodb.AWS.config.update({
+                        region: `${process.env.REGION}`
+                    });
+                }
+                this.clashSubscriptionTable = dynamodb.define(this.tableName, {
+                    hashKey: 'userId',
+                    timestamps: true,
+                    schema: {
+                        userId: Joi.string(),
+                        serverName: Joi.string()
+                    }
                 });
+                resolve(true);
+            } catch (err) {
+                reject(err);
             }
-            this.clashSubscriptionTable = dynamodb.define(this.tableName, {
-                hashKey: 'userId'
-            });
         });
     }
 
     subscribe(id, server) {
         return new Promise((resolve, reject) => {
-           resolve({id: id, server: server});
+            let subscription = {
+                userId: id,
+                serverName: server
+            };
+            this.clashSubscriptionTable.create(subscription, (err, data) => {
+                if (err) reject(err);
+                else console.log(`Successfully saved subscription => ${JSON.stringify(data)}`);
+            })
+            resolve(subscription);
         });
     }
 
     unsubscribe(id, server) {
         return new Promise((resolve, reject) => {
-            resolve({id: id, server: server});
+            let subscription = {
+                userId: id,
+                serverName: server
+            };
+            this.clashSubscriptionTable.delete(subscription, (err, data) => {
+                if (err) reject(err);
+                else console.log(`Successfully deleted subscription.`);
+            })
+            resolve(subscription);
         });
     }
 }
--- dao/tests/clash-subscription-db-impl.test.js
+++ dao/tests/clash-subscription-db-impl.test.js
@@ -1,11 +1,60 @@
 const clashSubscriptionDbImpl = require('../clash-subscription-db-impl');
+const streamTest = require('streamtest');
+const dynamodb = require('dynamodb');
+const Joi = require('joi');
+
+jest.mock('dynamodb');
+
+beforeEach(() => {
+    jest.resetModules();
+    delete process.env.LOCAL;
+});
+
+describe('Initialize Table connection', () => {
+    test('Initialize the table connection to be used.', async () => {
+        dynamodb.define = jest.fn();
+        return clashSubscriptionDbImpl.initialize().then(() => {
+            expect(dynamodb.AWS.config.loadFromPath.mock.calls.length).toEqual(0);
+            expect(dynamodb.AWS.config.update.mock.calls.length).toEqual(1);
+            expect(dynamodb.define).toBeCalledTimes(1);
+            expect(dynamodb.define).toBeCalledWith(clashSubscriptionDbImpl.tableName,
+                {
+                    hashKey: 'userId',
+                    timestamps: true,
+                    schema: {userId: Joi.string(), serverName: Joi.string()}
+                });
+        });
+    })
+
+    test('Initialize the table connection to be used with Local.', () => {
+        dynamodb.AWS.config.loadFromPath = jest.fn();
+        dynamodb.AWS.config.update = jest.fn();
+        dynamodb.define = jest.fn();
+        process.env.LOCAL = true;
+        return clashSubscriptionDbImpl.initialize().then((data) => {
+                expect(dynamodb.AWS.config.loadFromPath.mock.calls.length).toEqual(1);
+                expect(dynamodb.AWS.config.update.mock.calls.length).toEqual(0);
+                expect(dynamodb.define.mock.calls.length).toEqual(1);
+            }
+        )
+    })
+})
 
 describe('Subscribe', () => {
     test('Subscribe should be passed with a user id and a Server.', async () => {
         let id = '12345667';
         let server = 'TestServer';
+        let expectedResults = {
+            userId: id,
+            serverName: server
+        };
+        clashSubscriptionDbImpl.clashSubscriptionTable = jest.fn();
+        clashSubscriptionDbImpl.clashSubscriptionTable.create = jest.fn().mockImplementation((sub, callback) => {
+            callback(undefined, expectedResults)
+        });
         return clashSubscriptionDbImpl.subscribe(id, server).then(data => {
-            expect(data).toBeTruthy();
+            expect(data).toEqual(expectedResults);
+            expect(clashSubscriptionDbImpl.clashSubscriptionTable.create).toBeCalledTimes(1);
         });
     })
 })
@@ -14,8 +63,17 @@ describe('Unsubscribe', () => {
     test('Unsubscribe should be passed with a user id and a Server.', async () => {
         let id = '12345667';
         let server = 'TestServer';
+        let expectedResults = {
+            userId: id,
+            serverName: server
+        };
+        clashSubscriptionDbImpl.clashSubscriptionTable = jest.fn();
+        clashSubscriptionDbImpl.clashSubscriptionTable.delete = jest.fn().mockImplementation((sub, callback) => {
+            callback(undefined, expectedResults)
+        });
         return clashSubscriptionDbImpl.unsubscribe(id, server).then(data => {
-            expect(data).toBeTruthy();
+            expect(data).toEqual(expectedResults);
+            expect(clashSubscriptionDbImpl.clashSubscriptionTable.delete).toBeCalledTimes(1);
         });
     })
 })
