3d352d54c2f87f4cded0d57a59ba9ff4fb9b9c71
Daniel Poss <dpatposs@msn.com>
2021-07-15 14:18:47 -0500

    unsubscribe.js - adding check if delete function returns undefined then it shows that no subscription was found

--- commands/tests/unsubscribe.test.js
+++ commands/tests/unsubscribe.test.js
@@ -8,7 +8,7 @@ jest.mock('../../utility/error-handling');
 describe('Unsubscribe', () => {
 
     test('When a user requests to unsubscribe, they should have their ServerName and Id passed along to be persisted then responded with a message letting them know it was successful.', async () => {
-        let messagePassed;
+        let messagePassed = '';
         let msg = {
             reply: (value) => messagePassed = value,
             author: {
@@ -24,6 +24,25 @@ describe('Unsubscribe', () => {
 
         await unsubscribe.execute(msg);
         expect(messagePassed).toEqual('You have successfully unsubscribed.');
+    })
+
+    test('When a user requests to unsubscribe and they were not subscribed to begin with, they should have a message letting them know they were not subscribed.', async () => {
+        let messagePassed = '';
+        let msg = {
+            reply: (value) => messagePassed = value,
+            author: {
+                username: 'TestPlayer',
+                id: '123456789'
+            },
+            guild: {
+                name: 'TestServer'
+            }
+        };
+
+        clashSubscriptionDbImpl.unsubscribe = jest.fn().mockResolvedValue(undefined);
+
+        await unsubscribe.execute(msg);
+        expect(messagePassed).toEqual('No subscription was found.');
     })
 })
 
@@ -47,7 +66,7 @@ describe('Error', () => {
 
         await unsubscribe.execute(msg);
         expect(clashSubscriptionDbImpl.unsubscribe).toBeCalledTimes(1);
-        expect(clashSubscriptionDbImpl.unsubscribe).toBeCalledWith(msg.author.id, msg.guild.name);
+        expect(clashSubscriptionDbImpl.unsubscribe).toBeCalledWith(msg.author.id);
         expect(errorHandling.handleError.mock.calls.length).toEqual(1);
     })
 })
--- commands/unsubscribe.js
+++ commands/unsubscribe.js
@@ -7,13 +7,13 @@ module.exports = {
     description: 'Unsubscribes a user to a scheduled DM in Discord to notify them of an upcoming League of Legends Clash Tournament.',
     execute: async function (msg) {
         const startTime = process.hrtime.bigint();
-        await clashSubscriptionDbImpl.unsubscribe(msg.author.id, msg.guild.name).then(() => {
-            msg.reply('You have successfully unsubscribed.');
-        })
-        .catch(err => errorHandler.handleError(this.name, err, msg, 'Failed to unsubscribe.'))
-        .finally(() => {
-            timeTracker.endExecution(this.name, startTime);
-        });
+        await clashSubscriptionDbImpl.unsubscribe(msg.author.id)
+            .then((data) => data ? msg.reply('You have successfully unsubscribed.')
+                : msg.reply('No subscription was found.'))
+            .catch(err => errorHandler.handleError(this.name, err, msg, 'Failed to unsubscribe.'))
+            .finally(() => {
+                timeTracker.endExecution(this.name, startTime);
+            });
 
     },
 };
