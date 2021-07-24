7cb89ffe1160dbdede701f3eeb52ad1116df7278
Daniel Poss <dpatposs@msn.com>
2021-07-24 07:54:30 -0500

    Updated formatting for champions return.

--- commands/champions.js
+++ commands/champions.js
@@ -5,31 +5,40 @@ const riotApi = require('@fightmegg/riot-api');
 module.exports = {
     name: 'champions',
     description: 'Returns a description of the requested League of Legends Champions based on what the user requests.',
-    async execute(msg) {
+    async execute(msg, args) {
         const startTime = process.hrtime.bigint();
         try {
             const ddragon = new riotApi.DDragon();
             let champions = await ddragon.champion.all();
             let championKeys = Object.keys(champions.data);
-            console.log(`Number of Champions returned ('${championKeys.length}')`);
-            console.log(`List of Champion Keys returned ('${championKeys}')`);
-            let embeddedMessages = [];
-            for (let i = 0; i < 5; i++) {
-                let championName = championKeys[i];
-                let championData = await ddragon.champion.byName({championName: championName});
-                console.log(`Creating message for => ('${championName}')`)
-                embeddedMessages.push(templateBuilder.buildMessage(championTemplate, {
-                    championName: championName,
-                    championTitle: championData.data[championName].title,
-                    lore: championData.data[championName].lore,
-                    tag: championData.data[championName].tags[1] ? `${championData.data[championName].tags[0]} - ${championData.data[championName].tags[1]}` : championData.data[championName].tags[0],
-                    imageUrl: championData.data[championName].image.full,
-                    version: championData.version
-                }));
+            if (Array.isArray(args) && args[0]) {
+                console.log(`Filtering champion list for ('${args[0]}')`);
+                championKeys = championKeys.filter(championName => championName.toLowerCase().includes(args[0].toLowerCase()));
             }
-            embeddedMessages.forEach((message) => {
-                msg.member.send({embed: message});
-            })
+            if (championKeys.length > 0) {
+                console.log(`Number of Champions returned ('${championKeys.length}')`);
+                console.log(`List of Champion Keys returned ('${championKeys}')`);
+                let embeddedMessages = [];
+                for (let i = 0; i < (championKeys.length > 5 ? 5 : championKeys.length); i++) {
+                    let championName = championKeys[i];
+                    let championData = await ddragon.champion.byName({championName: championName});
+                    console.log(`Creating message for => ('${championName}')`)
+                    embeddedMessages.push(templateBuilder.buildMessage(championTemplate, {
+                        championName: championName,
+                        championTitle: championData.data[championName].title,
+                        lore: championData.data[championName].lore,
+                        tag: championData.data[championName].tags[1] ? `${championData.data[championName].tags[0]} - ${championData.data[championName].tags[1]}` : championData.data[championName].tags[0],
+                        imageUrl: championData.data[championName].image.full,
+                        version: championData.version
+                    }));
+                }
+                embeddedMessages.forEach((message) => {
+                    msg.member.send({embed: message});
+                })
+            } else {
+                msg.reply('Could not find the champion specified.')
+            }
+
         } finally {
             timeTracker.endExecution(this.name, startTime);
         }
--- commands/tests/champions.test.js
+++ commands/tests/champions.test.js
@@ -12,83 +12,142 @@ const aniviaData = require('./test-data/anivia-data');
 jest.mock('@fightmegg/riot-api/');
 
 describe('Champions Command', () => {
-    function getChampionDataByName(championName) {
-        let championData = {};
-        switch (championName) {
-            case 'Aatrox':
-                championData = aatroxData;
-                break;
-            case 'Ahri':
-                championData = ahriData;
-                break;
-            case 'Akali':
-                championData = akaliData;
-                break;
-            case 'Alistar':
-                championData = alistarData;
-                break;
-            case 'Anivia':
-                championData = aniviaData;
-                break;
-        }
-        return championData;
-    }
-
-    function getChampionData(championName) {
-        const championData = getChampionDataByName(championName);
-        return {
-            championName: championName,
-            championTitle: championData.data[championName].title,
-            lore: championData.data[championName].lore,
-            tag: championData.data[championName].tags[1] ?
-                `${championData.data[championName].tags[0]} - ${championData.data[championName].tags[1]}`
-                : championData.data[championName].tags[0],
-            imageUrl: championData.data[championName].image.full,
-            version: championData.version
-        };
-    }
-
-    function buildListOfChampionData(championName) {
-        return {id: championName};
-    }
-
-    function buildEmbeddedMessage(dataAnivia) {
-        return {embed: templateBuilder.buildMessage(template, dataAnivia)};
-    }
-
-    function buildExpectedChampionReturn(expectedChampionNameArray) {
-        let expectedArguments = [];
-        let expectedChampionsData = {data: {}};
-        for (const championNameIndex in expectedChampionNameArray) {
-            expectedChampionsData.data[expectedChampionNameArray[championNameIndex]] = buildListOfChampionData(expectedChampionNameArray[championNameIndex]);
-            expectedArguments.push([buildEmbeddedMessage(getChampionData(expectedChampionNameArray[championNameIndex]))]);
-        }
-        riotApi.DDragon = jest.fn().mockImplementation(() => {
-            return {
-                champion: {
-                    all: jest.fn().mockReturnValue(expectedChampionsData),
-                    byName: jest.fn().mockImplementation(({championName: name}) => {
-                        return getChampionDataByName(name);
-                    })
-                }
-            };
-        });
-        return expectedArguments;
-    }
 
     test('The champions command should return an embedded message with at most 5 champions specified to the specific user.', async () => {
-        let mockSendFunction = jest.fn();
         let msg = {
             member: {
-                send: mockSendFunction
+                send: jest.fn()
             }
         };
 
-        const expectedChampionNameArray = ['Aatrox', 'Ahri', 'Akali', 'Alistar', 'Anivia'];
-        let expectedArguments = buildExpectedChampionReturn(expectedChampionNameArray);
+        let expectedArguments = buildExpectedChampionList();
 
         await champions.execute(msg);
-        expect(mockSendFunction.mock.calls.length).toEqual(expectedArguments.length);
-        expect(mockSendFunction.mock.calls).toEqual(expectedArguments);
+        expect(msg.member.send.mock.calls).toEqual(expectedArguments);
+    })
+
+    test('The champions command should return a specific champion if one is passed.', async () => {
+        let msg = {
+            member: {
+                send: jest.fn()
+            }
+        };
+
+        let expectedArguments = buildExpectedChampionList();
+        let expectedMessage = expectedArguments.filter(record => record[0].embed.title.includes('Aatrox'));
+
+        await champions.execute(msg, ['Aatrox']);
+        expect(msg.member.send.mock.calls).toEqual(expectedMessage);
+    })
+
+    test('The champions command should return a specific champion if one is partially passed.', async () => {
+        let msg = {
+            member: {
+                send: jest.fn()
+            },
+            reply: jest.fn()
+        };
+
+        let expectedArguments = buildExpectedChampionList();
+        let expectedMessage = expectedArguments.filter(record => record[0].embed.title.includes('Aatrox'));
+
+        await champions.execute(msg, ['Aa']);
+        expect(msg.member.send.mock.calls).toEqual(expectedMessage);
+    })
+
+    test('The champions command should return a specific champion if one is in a different case is passed.', async () => {
+        let msg = {
+            member: {
+                send: jest.fn()
+            },
+            reply: jest.fn()
+        };
+
+        let expectedArguments = buildExpectedChampionList();
+        let expectedMessage = expectedArguments.filter(record => record[0].embed.title.includes('Aatrox'));
+
+        await champions.execute(msg, ['aa']);
+        expect(msg.member.send.mock.calls).toEqual(expectedMessage);
+    })
+
+    test('If the champion they specified is not found, then send a response to let them know.', async () => {
+        let msg = {
+            member: {
+                send: jest.fn()
+            },
+            reply: jest.fn()
+        };
+        buildExpectedChampionList();
+
+        await champions.execute(msg, ['DNE']);
+        expect(msg.reply.mock.calls).toEqual([['Could not find the champion specified.']]);
     })
 })
+
+function buildExpectedChampionList() {
+    return buildExpectedChampionReturn(['Aatrox', 'Ahri', 'Akali', 'Alistar', 'Anivia']);
+}
+
+function getChampionDataByName(championName) {
+    let championData = {};
+    switch (championName) {
+        case 'Aatrox':
+            championData = aatroxData;
+            break;
+        case 'Ahri':
+            championData = ahriData;
+            break;
+        case 'Akali':
+            championData = akaliData;
+            break;
+        case 'Alistar':
+            championData = alistarData;
+            break;
+        case 'Anivia':
+            championData = aniviaData;
+            break;
+    }
+    return championData;
+}
+
+function getChampionData(championName) {
+    const championData = getChampionDataByName(championName);
+    return {
+        championName: championName,
+        championTitle: championData.data[championName].title,
+        lore: championData.data[championName].lore,
+        tag: championData.data[championName].tags[1] ?
+            `${championData.data[championName].tags[0]} - ${championData.data[championName].tags[1]}`
+            : championData.data[championName].tags[0],
+        imageUrl: championData.data[championName].image.full,
+        version: championData.version
+    };
+}
+
+function buildListOfChampionData(championName) {
+    return {id: championName};
+}
+
+function buildEmbeddedMessage(dataAnivia) {
+    return {embed: templateBuilder.buildMessage(template, dataAnivia)};
+}
+
+function buildExpectedChampionReturn(expectedChampionNameArray) {
+    let expectedArguments = [];
+    let expectedChampionsData = {data: {}};
+    for (const championNameIndex in expectedChampionNameArray) {
+        expectedChampionsData.data[expectedChampionNameArray[championNameIndex]] = buildListOfChampionData(expectedChampionNameArray[championNameIndex]);
+        expectedArguments.push([buildEmbeddedMessage(getChampionData(expectedChampionNameArray[championNameIndex]))]);
+    }
+    riotApi.DDragon = jest.fn().mockImplementation(() => {
+        return {
+            champion: {
+                all: jest.fn().mockReturnValue(expectedChampionsData),
+                byName: jest.fn().mockImplementation(({championName: name}) => {
+                    return getChampionDataByName(name);
+                })
+            }
+        };
+    });
+    return expectedArguments;
+}
