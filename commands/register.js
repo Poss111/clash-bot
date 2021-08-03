e9eff8ea9cd697295b8648543d20c508043598c7
Daniel Poss <dpatposs@msn.com>
2021-08-03 15:35:35 -0500

    - Closes #54 - Updated the message for users who try to use the register command.

--- commands/register.js
+++ commands/register.js
@@ -6,7 +6,7 @@ module.exports = {
     execute: async function (msg) {
         const startTime = process.hrtime.bigint();
         try {
-            msg.reply('Register is going to be removed. ***newTeam*** and ***join*** are now the go to commands. Please use !clash help if you would like to view their usage.');
+            msg.reply('Register is going to be removed. ***newTeam*** (If you want to create a brand new team) and ***join*** (If you want to join an existing one, the team must exist) are now the go to commands. Please use !clash help if you would like to view their usage.');
         } finally {
             timeTracker.endExecution(this.name, startTime);
         }
