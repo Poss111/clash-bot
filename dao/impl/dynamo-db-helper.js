c939f781648f0fb2e9f3d7a37376f25cfa0d2309
Daniel Poss <dpatposs@msn.com>
2021-08-03 13:05:25 -0500

    - Testing if we can connect to localhost 8000

--- dao/impl/dynamo-db-helper.js
+++ dao/impl/dynamo-db-helper.js
@@ -11,10 +11,10 @@ class DynamoDbHelper {
                     console.log('Loading credentials for Integration Test.');
                     dynamodb.AWS.config.update({
                         region: `${process.env.REGION}`,
-                        endpoint: process.env.IP_ADDRESS ? `http://${process.env.IP_ADDRESS}:8000` : 'http://localhost:8000',
+                        endpoint: 'http://localhost:8000',
                         accessKeyId: 'Dummy',
                         secretAccessKey: 'Dummy'
-                    })
+                    });
                     this.setupConfig = true;
                 } else if (process.env.LOCAL) {
                     console.log('Loading credentials for local.');
