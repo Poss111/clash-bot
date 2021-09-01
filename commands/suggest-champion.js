7fce5d27c0be04a1c2fa9e2477d09b783892edd9
Daniel Poss <dpatposs@msn.com>
2021-09-01 15:58:49 -0500

    - Removed dependency to all dbs

--- commands/suggest-champion.js
+++ commands/suggest-champion.js
@@ -1,5 +1,5 @@
 const timeTracker = require('../utility/time-tracker');
-const clashSubscriptionDb = require('../dao/clash-subscription-db-impl');
+const userServiceImpl = require('../services/user-service-impl');
 const errorHandler = require('../utility/error-handling');
 const riotApi = require('@fightmegg/riot-api');
 module.exports = {
@@ -7,24 +7,29 @@ module.exports = {
     description: 'Adds or removes a champion to the players preferred Champions list.',
     async execute(msg, args) {
         const startTime = process.hrtime.bigint();
-        try {
             if (Array.isArray(args) && args.length < 1) {
                 msg.reply('no champion name was passed. Please pass one.');
             } else {
-                let ddragon = new riotApi.DDragon();
-                let championData = await ddragon.champion.all();
-                if (Object.keys(championData.data).find(record => record === args[0])) {
-                    await clashSubscriptionDb.updatePreferredChampions(msg.author.id, args[0], msg.guild.name).then(results => {
-                        msg.reply(`Successfully updated your preferred champions list, here are your current Champions: '${results.preferredChampions}'`);
-                    }).catch(err =>
-                        errorHandler.handleError(this.name, err, msg,
-                            'Failed to update the Users preferred Champions list.'));
-                } else {
-                    msg.reply(`Champion name passed does not exist. Please validate with !clash champions ${args[0]}`);
+                try {
+                    let ddragon = new riotApi.DDragon();
+                    let championData = await ddragon.champion.all();
+                    if (Object.keys(championData.data).find(record => record === args[0])) {
+                        let userDetails = await userServiceImpl.getUserDetails(msg.author.id);
+                        if (!Array.isArray(userDetails.preferredChampions)) {
+                            userDetails.preferredChampions = [args[0]];
+                        } else {
+                            userDetails.preferredChampions.push(args[0]);
+                        }
+                        let updatedUserDetails = await userServiceImpl.postUserDetails(msg.author.id, msg.author.username, userDetails.serverName, userDetails.preferredChampions, userDetails.subscriptions);
+                        msg.reply(`Successfully updated your preferred champions list, here are your current Champions: '${updatedUserDetails.preferredChampions}'`);
+                    } else {
+                        msg.reply(`Champion name passed does not exist. Please validate with !clash champions ${args[0]}`);
+                    }
+                } catch (err) {
+                    errorHandler.handleError(this.name, err, msg, 'Failed to update the Users preferred Champions list.');
+                } finally {
+                    timeTracker.endExecution(this.name, startTime);
                 }
             }
-        } finally {
-            timeTracker.endExecution(this.name, startTime);
-        }
     },
 };
--- commands/tests/suggest-champion.unit.test.js
+++ commands/tests/suggest-champion.unit.test.js
@@ -1,38 +1,96 @@
 const suggestChampion = require('../suggest-champion');
-const clashSubscriptionDb = require('../../dao/clash-subscription-db-impl');
+const userServiceImpl = require('../../services/user-service-impl');
 const errorHandler = require('../../utility/error-handling');
 const riotApi = require('@fightmegg/riot-api');
 
 jest.mock('../../dao/clash-subscription-db-impl');
+jest.mock('../../services/user-service-impl');
 jest.mock('../../utility/error-handling');
 jest.mock('@fightmegg/riot-api');
 
+beforeEach(() => {
+    jest.resetModules();
+    jest.resetAllMocks();
+})
+
 describe('Suggest Champion Command', () => {
     test('A User should be able to pass in a champion name as an argument to be added to their preferred champions list.', async () => {
+        const expectedPlayerId = '1';
+        const expectedPlayerName = 'Roidrage';
+        const expectedServerName = 'Goon Squad';
+        const expectedSubscriptions = { 'UpcomingClashTournamentDiscordDM': false };
         let msg = {
             author: {
                 username: 'Sample User',
                 id: '123456'
             },
             guild: {
-                name: 'Good Squad'
+                name: 'Goon Squad'
             },
             send: jest.fn(),
             reply: jest.fn()
         }
-        let args = ['Ahri'];
-        let expectedResults = {
-            key: msg.author.id,
-            serverName: msg.guild.name,
-            preferredChampions: args,
-            subscribed: false,
-            timeAdded: expect.any(String)
+        const mockGetUserResponse = {
+            id: expectedPlayerId,
+            playerName: expectedPlayerName,
+            serverName: expectedServerName,
+            preferredChampions: undefined,
+            subscriptions: expectedSubscriptions
         };
+
+        let args = ['Ahri'];
+        let mockPostUserResponse = JSON.parse(JSON.stringify(mockGetUserResponse));
+        mockPostUserResponse.preferredChampions = [];
+        mockPostUserResponse.preferredChampions.push(args[0]);
+
         prepareDDragonApiData();
-        clashSubscriptionDb.updatePreferredChampions = jest.fn().mockResolvedValue(expectedResults);
+        userServiceImpl.getUserDetails.mockResolvedValue(mockGetUserResponse);
+        userServiceImpl.postUserDetails.mockResolvedValue(mockPostUserResponse)
         await suggestChampion.execute(msg, args)
-        expect(msg.reply).toBeCalledWith(`Successfully updated your preferred champions list, here are your current Champions: '${expectedResults.preferredChampions}'`);
-        expect(clashSubscriptionDb.updatePreferredChampions).toBeCalledWith(msg.author.id, args[0], msg.guild.name);
+        expect(msg.reply).toBeCalledWith(`Successfully updated your preferred champions list, here are your current Champions: '${mockPostUserResponse.preferredChampions}'`);
+        expect(userServiceImpl.getUserDetails).toHaveBeenCalledTimes(1);
+        expect(userServiceImpl.getUserDetails).toBeCalledWith(msg.author.id);
+        expect(userServiceImpl.postUserDetails).toHaveBeenCalledTimes(1);
+        expect(userServiceImpl.postUserDetails).toBeCalledWith(msg.author.id, msg.author.username, expectedServerName, mockPostUserResponse.preferredChampions, mockGetUserResponse.subscriptions);
+    })
+
+    test('A User should be able to pass in a champion name as an argument to be added to their preferred champions list if their list is already populated.', async () => {
+        const expectedPlayerId = '1';
+        const expectedPlayerName = 'Roidrage';
+        const expectedServerName = 'Goon Squad';
+        const expectedSubscriptions = { 'UpcomingClashTournamentDiscordDM': false };
+        let msg = {
+            author: {
+                username: 'Sample User',
+                id: '123456'
+            },
+            guild: {
+                name: 'Goon Squad'
+            },
+            send: jest.fn(),
+            reply: jest.fn()
+        }
+        const mockGetUserResponse = {
+            id: expectedPlayerId,
+            playerName: expectedPlayerName,
+            serverName: expectedServerName,
+            preferredChampions: ['Sett'],
+            subscriptions: expectedSubscriptions
+        };
+
+        let args = ['Ahri'];
+        let mockPostUserResponse = JSON.parse(JSON.stringify(mockGetUserResponse));
+        mockPostUserResponse.preferredChampions.push(args[0]);
+
+        prepareDDragonApiData();
+        userServiceImpl.getUserDetails.mockResolvedValue(mockGetUserResponse);
+        userServiceImpl.postUserDetails.mockResolvedValue(mockPostUserResponse)
+        await suggestChampion.execute(msg, args)
+        expect(msg.reply).toBeCalledWith(`Successfully updated your preferred champions list, here are your current Champions: '${mockPostUserResponse.preferredChampions}'`);
+        expect(userServiceImpl.getUserDetails).toHaveBeenCalledTimes(1);
+        expect(userServiceImpl.getUserDetails).toBeCalledWith(msg.author.id);
+        expect(userServiceImpl.postUserDetails).toHaveBeenCalledTimes(1);
+        expect(userServiceImpl.postUserDetails).toBeCalledWith(msg.author.id, msg.author.username, expectedServerName, mockPostUserResponse.preferredChampions, mockGetUserResponse.subscriptions);
     })
 
     describe('Should validate user input', () => {
@@ -49,10 +107,9 @@ describe('Suggest Champion Command', () => {
                 reply: jest.fn()
             }
             let args = [];
-            clashSubscriptionDb.updatePreferredChampions = jest.fn();
             await suggestChampion.execute(msg, args)
             expect(msg.reply).toBeCalledWith('no champion name was passed. Please pass one.');
-            expect(clashSubscriptionDb.updatePreferredChampions).toBeCalledTimes(0);
+            expect(userServiceImpl.getUserDetails).not.toBeCalled();
         })
 
         test('The champion passed should be a valid League of Legends champion name.', async () => {
@@ -69,10 +126,9 @@ describe('Suggest Champion Command', () => {
             }
             let args = ['DNE'];
             prepareDDragonApiData();
-            clashSubscriptionDb.updatePreferredChampions = jest.fn();
             await suggestChampion.execute(msg, args)
             expect(msg.reply).toBeCalledWith(`Champion name passed does not exist. Please validate with !clash champions ${args[0]}`);
-            expect(clashSubscriptionDb.updatePreferredChampions).toBeCalledTimes(0);
+            expect(userServiceImpl.getUserDetails).not.toBeCalled();
         })
     })
 })
@@ -91,7 +147,7 @@ test('If an error occurs, the error handler will be invoked.', async () => {
         reply: jest.fn()
     }
     prepareDDragonApiData();
-    clashSubscriptionDb.updatePreferredChampions = jest.fn().mockRejectedValue(new Error('Failed to update.'));
+    userServiceImpl.getUserDetails.mockRejectedValue(new Error('Failed to update.'));
     errorHandler.handleError = jest.fn();
     await suggestChampion.execute(msg, ['Ahri']);
     expect(errorHandler.handleError.mock.calls.length).toEqual(1);
