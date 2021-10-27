fb4bbb2830aeb9d9c1ec776208620e3acc1c20b3
Daniel Poss <dpatposs@msn.com>
2021-10-27 09:51:40 -0500

    - Adding message to be sent when correct command is recognized to allow users to know that Clash-Bot has limited functionality during preseason

--- templates/update-notification.js
+++ templates/update-notification.js
@@ -4,7 +4,7 @@ module.exports = {
     description: "**We have a new site to go along with the Bot :man_dancing:**!! https://clash-bot.ninja " +
         "Please check the Releases page for new updates and bug fixes :smile:. Donations are always " +
         "welcome [Paypal](https://www.paypal.com/paypalme/poss11111), it takes :moneybag: to keep a bot " +
-        "alive these days. **NOTE** : League's Season is coming to a close and unfortunately, so are the " +
+        "alive these days. **NOTE** : League's Season 11 is coming to a close and unfortunately, so are the " +
         "Clash Tournaments for now so Clash Bot will have minimal usage for now. You can still use the champion" +
         "feature to learn more about the LoL Champions. See you all next year!",
     image: {
