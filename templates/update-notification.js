0bdc1734c96573a5bdd42fd80fa896fcf7b1cb95
Daniel Poss <dpatposs@msn.com>
2022-01-11 16:47:53 -0600

    - Removing preseason message from release notification

--- templates/update-notification.js
+++ templates/update-notification.js
@@ -4,9 +4,7 @@ module.exports = {
     description: "**We have a new site to go along with the Bot :man_dancing:**!! https://clash-bot.ninja " +
         "Please check the Releases page for new updates and bug fixes :smile:. Donations are always " +
         "welcome [Paypal](https://www.paypal.com/paypalme/poss11111), it takes :moneybag: to keep a bot " +
-        "alive these days. **NOTE** : League's Season 11 is coming to a close and unfortunately, so are the " +
-        "Clash Tournaments for now, so Clash Bot will have minimal usage for now. You can still use the champion" +
-        "feature to learn more about the LoL Champions. See you all next year!!",
+        "alive these days.",
     image: {
         url: "https://repository-images.githubusercontent.com/363187357/577557c6-50c4-422c-adbf-8a06281c14e9"
     },
