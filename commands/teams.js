ca7d670517adfabf050fc6abf0dd79b6a5fecab1
Poss111 <dpatposs@msn.com>
2022-01-30 14:34:11 -0600

    - Command teams - Refactored to use CommandInteraction

--- commands/teams.js
+++ commands/teams.js
@@ -3,35 +3,42 @@ const tentativeServiceImpl = require('../services/tentative-service-impl');
 const teamsCard = require('../templates/teams');
 const errorHandler = require('../utility/error-handling');
 const timeTracker = require('../utility/time-tracker');
+
 module.exports = {
     name: 'teams',
-    description: 'Used to return a human readable response of the current Clash team status.',
+    description: 'Used to return response of the current Clash team status.',
     execute: async function (msg) {
         const startTime = process.hrtime.bigint();
-        await Promise.all([tentativeServiceImpl.retrieveTentativeListForServer(msg.guild.name),
-            clashBotTeamsServiceImpl.retrieveActiveTeamsForServer(msg.guild.name)]).then(data => {
+        await msg.deferReply();
+        await Promise.all([tentativeServiceImpl.retrieveTentativeListForServer(msg.member.guild.name),
+            clashBotTeamsServiceImpl.retrieveActiveTeamsForServer(msg.member.guild.name)]).then(data => {
             let tentative = data[0];
+            let teams = data[1];
             let copy = JSON.parse(JSON.stringify(teamsCard));
 
-            if (data[1].length <= 0) {
+            if (teams.length <= 0) {
                 copy.fields.push({name: 'No Existing Teams. Please register!', value: 'Emptay'})
             } else {
                 let counter;
-                for (counter = 0; counter < data[1].length; counter++) {
-                    if (data[1][counter].playersDetails && data[1][counter].playersDetails.length > 0) {
-                        copy.fields.push({name: data[1][counter].teamName,
-                            value: data[1][counter].playersDetails.map(record => record.role + " - " + record.name), inline: true});
+                for (counter = 0; counter < teams.length; counter++) {
+                    if (teams[counter].playersDetails && teams[counter].playersDetails.length > 0) {
                         copy.fields.push({
-                            name: 'Tournament Details',
-                            value: `${data[1][counter].tournamentDetails.tournamentName} Day ${data[1][counter].tournamentDetails.tournamentDay}`,
+                            name: teams[counter].teamName,
+                            value: teams[counter].playersDetails.map(record => record.role + " - " + record.name),
                             inline: true
                         });
-                        if (counter < data[1].length - 1) {
+                        copy.fields.push({
+                            name: 'Tournament Details',
+                            value: `${teams[counter].tournamentDetails.tournamentName} Day ${teams[counter].tournamentDetails.tournamentDay}`,
+                            inline: true
+                        });
+                        if (counter < teams.length - 1) {
                             copy.fields.push({name: '\u200B', value: '\u200B'});
                         }
                     }
                 }
-            } if (tentative && tentative.length > 0) {
+            }
+            if (tentative && tentative.length > 0) {
                 const reduce = tentative.reduce((acc, value) => {
                     const key = `${value.tournamentDetails.tournamentName} - ${value.tournamentDetails.tournamentDay}`;
                     if (!acc[key]) {
@@ -52,7 +59,7 @@ module.exports = {
                 }
                 copy.fields.push({name: 'Tentative Queue', value: message});
             }
-            msg.reply({embed: copy});
+            msg.reply({embeds: [ copy ]});
         }).catch(err => errorHandler.handleError(this.name, err, msg, 'Failed to retrieve the current Clash Teams status.'))
             .finally(() => {
                 timeTracker.endExecution(this.name, startTime);
