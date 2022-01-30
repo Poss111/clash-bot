d0510d342c0ddf8f96b7dc107d3f32827a0b34de
Poss111 <dpatposs@msn.com>
2022-01-30 15:56:34 -0600

    - Command unregister - refactored time to use CommandInteraction

--- commands/unregister.js
+++ commands/unregister.js
@@ -41,34 +41,35 @@ module.exports = {
     ],
     execute: async function (msg, args) {
         const startTime = process.hrtime.bigint();
-        const parsedArguments = commandArgumentParser.parse(args);
-        if (!parsedArguments || (!parsedArguments.tournamentName || !parsedArguments.tournamentDay)) {
-            msg.reply(`Please pass the tournament and day to unregister for i.e. !clash unregister ***msi2021*** ***2***`);
-            timeTracker.endExecution(this.name, startTime);
-        } else {
-            try {
+        try {
+            const parsedArguments = commandArgumentParser.parse(args);
+            if (!parsedArguments || (!parsedArguments.tournamentName || !parsedArguments.tournamentDay)) {
+                msg.reply(`Please pass the tournament and day to unregister for i.e. !clash unregister ***msi2021*** ***2***`);
+                timeTracker.endExecution(this.name, startTime);
+            } else {
+                await msg.deferReply();
                 let times = await tournamentsServiceImpl.retrieveAllActiveTournaments(parsedArguments.tournamentName,
                     parsedArguments.tournamentDay);
                 if (Array.isArray(times) && times.length) {
-                    msg.channel.send(`Unregistering ${msg.author.username} from Tournament ${times[0].tournamentName} on Day ${times[0].tournamentDay}...`)
-                    let data = await teamsServiceImpl.deleteFromTeam(msg.author.id, msg.guild.name,
+                    await msg.reply(`Unregistering '${msg.user.username}' from Tournament '${times[0].tournamentName}' on day '${times[0].tournamentDay}'...`)
+                    let data = await teamsServiceImpl.deleteFromTeam(msg.user.id, msg.member.guild.name,
                         times[0].tournamentName, times[0].tournamentDay);
                     if (!data.error) {
-                        msg.reply("Removed you from your Team. Please use !clash join or !clash newTeam if you " +
+                        await msg.editReply("Removed you from your Team. Please use !clash join or !clash newTeam if you " +
                             "would like to join again. Thank you!")
                     } else {
-                        msg.reply("We did not find you on an existing Team. Please use !clash join or !clash newTeam " +
+                        await msg.editReply("We did not find you on an existing Team. Please use !clash join or !clash newTeam " +
                             "if you would like to join again. Thank you!");
                     }
                 } else {
-                    msg.reply("Please provide an existing tournament and day to unregister for. " +
+                    await msg.reply("Please provide an existing tournament and day to unregister for. " +
                         "Use '!clash team' to print a team.");
                 }
-            } catch (err) {
-                errorHandler.handleError(this.name, err, msg, 'Failed to unregister you.')
-            } finally {
-                timeTracker.endExecution(this.name, startTime);
             }
+        } catch (err) {
+            await errorHandler.handleError(this.name, err, msg, 'Failed to unregister you.')
+        } finally {
+            timeTracker.endExecution(this.name, startTime);
         }
     },
 };
