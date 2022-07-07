fb8a4c12632f76b13fb437792de8505daccfe7b9
Poss111 <dpatposs@msn.com>
2022-07-06 20:41:41 -0500

    Adding in ECS service

--- tfe/main.tf
+++ tfe/main.tf
@@ -6,4 +6,23 @@ terraform {
       name = "ClashBot"
     }
   }
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "4.21.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_ecs_cluster" "clash-bot-cluster" {
+  name = "clash-bot-tfe-ecs"
+
+  setting {
+    name  = "containerInsights"
+    value = "enabled"
+  }
 }
