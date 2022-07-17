cb298d2af2af360d82f3a03270d69431b10436e5
Poss111 <dpatposs@msn.com>
2022-07-17 15:02:31 -0500

    - Fixing shell-script

--- get_latest_image.sh
+++ get_latest_image.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo "Running..."
 output=$(aws ecr describe-images --repository-name poss11111 --query 'sort_by(imageDetails,& imagePushedAt)[-1]')
-echo "Timestamp for image: $($IMAGE_RESPONSE | jq -r '.imagePushedAt')"
-echo "IMAGE=$ECR_REGISTRY/$ECR_REPOSITORY:$($IMAGE_RESPONSE | jq -r '.imageTags[0]')" >> $GITHUB_ENV
+echo "Timestamp for image: $(echo $output | jq -r '.imagePushedAt')"
+echo "IMAGE=$ECR_REGISTRY/$ECR_REPOSITORY:$(echo $output | jq -r '.imageTags[0]')" >> $GITHUB_ENV
 echo "Finished"
