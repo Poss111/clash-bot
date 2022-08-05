3b8eb2a06673799059958630a6633a2d6dcf4039
Daniel Poss <dpatposs@msn.com>
2022-08-05 07:07:22 -0500

    - Swapping format of output keys

--- tfe/outputs.tf
+++ tfe/outputs.tf
@@ -1,9 +1,9 @@
-output "ecs-name" {
+output "ecs_name" {
   value       = aws_ecs_cluster.clash-bot-ecs.name
   description = "ECS Name"
 }
 
-output "ecs-id" {
+output "ecs_id" {
   value       = aws_ecs_cluster.clash-bot-ecs.id
   description = "ECS Id"
 }
